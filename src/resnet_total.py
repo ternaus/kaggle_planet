d299498b98f84f938b0f2d113a771b0388cb8d9b
Vladimir Iglovikov <iglovikov@gmail.com>
2017-06-28 20:38:19 -0700

    Added parallelization on 2 GPUs

--- src/resnet_total.py
+++ src/resnet_total.py
@@ -9,7 +9,6 @@ import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 from tqdm import tqdm
 import cv2
-from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense
@@ -17,6 +16,58 @@ from keras.models import Model
 import h5py
 import datetime
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import merge
+import tensorflow as tf
+from keras.layers.core import Lambda
+
+
+def make_parallel(model, gpu_count):
+    __author__ = "kuza55"
+
+    # https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
+
+    def get_slice(data, idx, parts):
+        shape = tf.shape(data)
+        size = tf.concat([shape[:1] // parts, shape[1:]], axis=0)
+        stride = tf.concat([shape[:1] // parts, shape[1:] * 0], axis=0)
+        start = stride * idx
+        return tf.slice(data, start, size)
+
+    outputs_all = []
+    for i in range(len(model.outputs)):
+        outputs_all.append([])
+
+    # Place a copy of the model on each GPU, each getting a slice of the batch
+    for i in range(gpu_count):
+        with tf.device('/gpu:%d' % i):
+            with tf.name_scope('tower_%d' % i) as scope:
+
+                inputs = []
+                # Slice each input into a piece for processing on this GPU
+                for x in model.inputs:
+                    input_shape = tuple(x.get_shape().as_list())[1:]
+                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx': i, 'parts': gpu_count})(x)
+                    inputs.append(slice_n)
+
+                outputs = model(inputs)
+
+                if not isinstance(outputs, list):
+                    outputs = [outputs]
+
+                # Save all the outputs for merging back together later
+                for l in range(len(outputs)):
+                    outputs_all[l].append(outputs[l])
+
+    # merge outputs on CPU
+    with tf.device('/cpu:0'):
+        merged = []
+        if len(outputs_all) == 1:
+            merged.append(merge(outputs_all[0], mode='concat', concat_axis=0, name='output'))
+        else:
+            for outputs in outputs_all:
+                merged.append(merge(outputs, mode='concat', concat_axis=0))
+
+        return Model(input=model.inputs, output=merged)
 
 
 def get_model():
@@ -70,7 +121,7 @@ if __name__ == '__main__':
     random_state = 2016
     data_path = '../data'
 
-    model = get_model()
+    model = make_parallel(get_model(), 2)
     print('[{}] Compiling model...'.format(str(datetime.datetime.now())))
     model.compile(optimizer='adam', loss=total_loss, metrics=['accuracy', fbeta, 'binary_crossentropy'])
 
