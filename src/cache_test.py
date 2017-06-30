b37b229ee162e5fec5c94b8672ed17e3367178df
Vladimir Iglovikov <iglovikov@gmail.com>
2017-06-29 21:51:03 -0700

    Added file that caches test

--- src/cache_test.py
+++ src/cache_test.py
@@ -17,11 +17,21 @@ num_test = len(os.listdir(test_path))
 
 f = h5py.File(os.path.join(data_path, 'test_jpg.h5'), 'w', compression='blosc:lz4', compression_opts=9)
 
-imgs = f.create_dataset('X', (num_test, 256, 256, 3), dtype=np.uint8)
+imgs = f.create_dataset('X', (num_test, 256, 256, 3), dtype=np.float16)
+
+image_names = []
 
 for i, file_name in enumerate(tqdm(os.listdir(test_path))):
-    img = cv2.imread(os.path.join(test_path, file_name))
+    img = cv2.imread(os.path.join(test_path, file_name)).astype(np.float16)
 
-    imgs[i] = img
+    img[:, :, 0] -= 103.939
+    img[:, :, 1] -= 116.779
+    img[:, :, 2] -= 123.68
+
+    imgs[i] = img.astype(np.float16)
+
+    image_names += [file_name]
+
+f['file_name'] = image_names
 
 f.close()
