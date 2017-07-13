3d2b55736ea7fb537fded47b323e1566542d8269
Vladimir Iglovikov <viglovikov@trueaccord.com>
2017-07-13 15:05:31 -0700

    Added tiff as data source

--- src/data_loader.py
+++ src/data_loader.py
@@ -11,30 +11,57 @@ from PIL import Image
 
 import torch.utils.data as data
 import torchvision.transforms as transforms
+import tifffile as tiff
 
 
 class CSVDataset(data.Dataset):
-  def __init__(self, path, transform=None):
-    df = pd.read_csv(path)
-    self.path = df.iloc[:, 0].values.astype(str)
-    self.target = df.iloc[:, 1:].values.astype(np.float32)
-    self.transform = transform
+    def __init__(self, path, transform=None):
+        df = pd.read_csv(path)
+        self.path = df.iloc[:, 0].values.astype(str)
+        self.target = df.iloc[:, 1:].values.astype(np.float32)
+        self.transform = transform
 
-  def __len__(self):
-    return self.target.shape[0]
+    def __len__(self):
+        return self.target.shape[0]
 
-  @staticmethod
-  def _load_pil(path):
-    with open(path, 'rb') as f:
-      with Image.open(f) as img:
-        return img.convert('RGB')
+    @staticmethod
+    def _load_pil(path):
+        with open(path, 'rb') as f:
+            with Image.open(f) as img:
+                return img.convert('RGB')
 
-  def __getitem__(self, idx):
-    X = self._load_pil(self.path[idx])
-    if self.transform:
-      X = self.transform(X)
-    y = self.target[idx]
-    return X, y
+    def __getitem__(self, idx):
+        X = self._load_pil(self.path[idx])
+        if self.transform:
+            X = self.transform(X)
+        y = self.target[idx]
+        return X, y
+
+
+class CSVDatasetTiff(data.Dataset):
+    def __init__(self, path, transform=None):
+        df = pd.read_csv(path)
+        self.path = df.iloc[:, 0].values.astype(str)
+        self.target = df.iloc[:, 1:].values.astype(np.float32)
+        self.transform = transform
+
+    def __len__(self):
+        return self.target.shape[0]
+
+    @staticmethod
+    def _load_pil(path):
+        tf = tiff.imread(path)
+        img = tf[:, :, [3, 0, 1]]
+        img = (1.0 * img / (2 ** 8 + 1)).astype(np.uint8)
+
+        return Image.fromarray(img)
+
+    def __getitem__(self, idx):
+        X = self._load_pil(self.path[idx])
+        if self.transform:
+            X = self.transform(X)
+        y = self.target[idx]
+        return X, y
 
 
 def get_loaders(batch_size,
@@ -66,6 +93,36 @@ def get_loaders(batch_size,
         ])
 
     valid_dataset = CSVDataset(f'../data/fold{fold}/val.csv', transform=valid_transform)
+    valid_loader = data.DataLoader(valid_dataset,
+                                   batch_size=batch_size,
+                                   shuffle=False,
+                                   num_workers=8,
+                                   pin_memory=True)
+
+    return train_loader, valid_loader
+
+
+def get_loaders_tiff(batch_size,
+                     fold=0,
+                     train_transform=None,
+                     valid_transform=None):
+
+    train_dataset = CSVDatasetTiff(f'../data/fold{fold}/train.csv', transform=train_transform)
+    train_loader = data.DataLoader(train_dataset,
+                                   batch_size=batch_size,
+                                   shuffle=True,
+                                   num_workers=8,
+                                   pin_memory=True)
+
+    if not valid_transform:
+        valid_transform = transforms.Compose([
+          transforms.Scale(256),
+          transforms.CenterCrop(224),
+          transforms.ToTensor(),
+          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    valid_dataset = CSVDatasetTiff(f'../data/fold{fold}/val.csv', transform=valid_transform)
     valid_loader = data.DataLoader(valid_dataset,
                                    batch_size=batch_size,
                                    shuffle=False,
---
+++ src/pt_model_tiff.py
@@ -0,0 +1,148 @@
+"""
+Experiments with pytorch
+"""
+
+
+import torch.optim as optim
+import pandas as pd
+from torch import np  # Torch wrapper for Numpy
+
+import os
+from PIL import Image
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import MultiLabelSoftMarginLoss
+
+from torchvision.models import resnet50, resnet101, resnet152
+from torchvision.models import densenet121, densenet161, densenet201
+
+import torch.nn.functional as F
+import utils
+import tqdm
+
+from sklearn.metrics import fbeta_score
+import numpy as np
+import shutil
+import argparse
+from torch.optim import Adam
+import data_loader
+import augmentations
+
+
+def f2_score(y_true, y_pred):
+    # fbeta_score throws a confusing error if inputs are not numpy arrays
+    y_true, y_pred, = np.array(y_true), np.array(y_pred)
+    # We need to use average='samples' here, any other average method will generate bogus results
+    return fbeta_score(y_true, y_pred, beta=2, average='samples')
+
+
+def validation(model, criterion, valid_loader):
+    model.eval()
+    losses = []
+    f2_scores = []
+    for inputs, targets in valid_loader:
+        inputs = utils.variable(inputs, volatile=True)
+        targets = utils.variable(targets)
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        losses.append(loss.data[0])
+        f2_scores.append(f2_score(y_true=targets.data.cpu().numpy(), y_pred=F.sigmoid(outputs).data.cpu().numpy() > 0.2))
+    valid_loss = np.mean(losses)  # type: float
+    valid_f2 = np.mean(f2_scores)  # type: float
+    print('Valid loss: {:.4f}, F2: {:.4f}'.format(valid_loss, valid_f2))
+    return {'valid_loss': valid_loss, 'valid_f2': valid_f2}
+
+
+def get_model(num_classes, model_type='resnet50'):
+    if model_type == 'resnet50':
+        model = resnet50(pretrained=True).cuda()
+        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
+    elif model_type == 'resnet101':
+        model = resnet101(pretrained=True).cuda()
+        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
+    elif model_type == 'resnet152':
+        model = resnet152(pretrained=True).cuda()
+        model.fc = nn.Linear(model.fc.in_features, num_classes).cuda()
+    elif model_type == 'densenet121':
+        model = densenet121(pretrained=True).cuda()
+        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
+    elif model_type == 'densenet161':
+        model = densenet161(pretrained=True).cuda()
+        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
+    elif model_type == 'densenet201':
+        model = densenet201(pretrained=True).cuda()
+        model.classifier = nn.Linear(model.classifier.in_features, num_classes).cuda()
+    return model
+
+
+def add_args(parser):
+    arg = parser.add_argument
+    arg('root', help='checkpoint root')
+    arg('--batch-size', type=int, default=4)
+    arg('--n-epochs', type=int, default=30)
+    arg('--lr', type=float, default=0.0001)
+    arg('--workers', type=int, default=8)
+    arg('--fold', type=int, default=0)
+    arg('--n-folds', type=int, default=10)
+    arg('--clean', action='store_true')
+    arg('--epoch-size', type=int)
+    arg('--device-ids', type=str, help='For example 0,1 to run on two GPUs')
+
+
+if __name__ == '__main__':
+    random_state = 2016
+    model_name = 'densenet201'
+
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg('--mode', choices=['train', 'valid', 'predict_valid', 'predict_test'], default='train')
+    add_args(parser)
+    args = parser.parse_args()
+
+    batch_size = args.batch_size
+
+    train_transform = transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        augmentations.D4(),
+        # augmentations.Rotate(),
+        # augmentations.GaussianBlur(),
+        augmentations.Add(-5, 5, per_channel=True),
+        augmentations.ContrastNormalization(0.8, 1.2, per_channel=True),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    train_loader, valid_loader = data_loader.get_loaders(batch_size,
+                                                         train_transform=train_transform,
+                                                         fold=0)
+
+    num_classes = 17
+
+    model = get_model(num_classes, model_name)
+
+    if utils.cuda_is_available:
+        if args.device_ids:
+            device_ids = list(map(int, args.device_ids.split(',')))
+        else:
+            device_ids = None
+
+        model = nn.DataParallel(model, device_ids=device_ids).cuda()
+
+    criterion = MultiLabelSoftMarginLoss()
+
+    utils.train(
+        init_optimizer=lambda lr: Adam(model.parameters(), lr=lr),
+        args=args,
+        model=model,
+        criterion=criterion,
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+        validation=validation,
+        # save_predictions=save_predictions,
+        patience=2,
+    )
